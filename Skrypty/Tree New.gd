aabe40fb6de3b1b03aefd2b0efe292e4b80593a6
PiotrCzap <piotrczapmlawa@interia.pl>
2025-04-25 22:58:09 +0200

    Survival Game

--- Skrypty/Tree New.gd
+++ Skrypty/Tree New.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-var Stick_Max_Drop = 2
-var Wood_Max_Drop = 3
-var Wood_Saplings_Max_Drop = 3
-var Apple_Max_Drop = 3
+var Stick_Max_Drop: int = 2
+var Wood_Max_Drop: int = 3
+var Wood_Saplings_Max_Drop: int = 3
+var Apple_Max_Drop: int = 3
+
+var Tree_HP: int = 10
+var Item_FullL: bool = false
+
 @onready var Wood_Sound = $"../Tree Cutting Sound"
 
 func _ready():
@@ -14,27 +18,40 @@ func _ready():
 
 func _on_tree_pressed(tree):
 	Wood_Sound.play()
-	print(Global.Tree_HP)
+	print(Tree_HP)
 	if Global.Wood < Global.Wood_Item_Stack:
-		Global.Tree_HP -= 1
-	if Global.Wooden_Axe == 1:		
+		Tree_HP -= 1
+	if Global.Wooden_Axe == 1:
 		if Global.Wooden_Axe_Equipped == true:
-			Global.Tree_HP -= 2
+			Tree_HP -= 2
 	
 	
 	# Sprawdzenie, czy HP osiągnęło 0
-	if Global.Tree_HP <= 0:
+	if Tree_HP <= 0:
 		tree.queue_free()  # Usunięcie drzewa
-		Global.Tree_HP = 10  # Resetowanie HP do 10
-		Global.Sticks += randi_range(1, Stick_Max_Drop)
-		Global.Wood += randi_range(1, Wood_Max_Drop)
+		Tree_HP = 10  # Resetowanie HP do 10
+		if Global.Sticks < 32:
+			Global.Sticks += randi_range(1, Stick_Max_Drop)
+		if Global.Wood < 32:
+			Global.Wood += randi_range(1, Wood_Max_Drop)
 		Global.Wood_Saplings += randi_range(1, Wood_Saplings_Max_Drop)
-		Global.Apples += randi_range(1, Apple_Max_Drop)
+		if Global.Apples < 32:
+			Global.Apples += randi_range(1, Apple_Max_Drop)
 		$"../../Player/Level Progress Bar".value += randi_range(2, 3)
+		if Global.Wood >= 32:
+			Set_Wood()
+		if Global.Sticks >= 32:
+			Set_Sticks()
+		if Global.Apples >= 32:
+			Set_Apples()
 
 
-func _process(delta: float) -> void:
-	if Global.Wood >= 32:
-		Global.Wood = 32
-	if Global.Apples >= 32:
-		Global.Apples = 32
+
+func Set_Wood():
+	Global.Wood = 32
+
+func Set_Sticks():
+	Global.Sticks = 32
+
+func Set_Apples():
+	Global.Apples = 32
